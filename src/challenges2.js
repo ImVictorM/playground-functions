741aa82097529cfa5e608992b5cad317fe4ada13
Victor Mendes <victorutdsim7@gmail.com>
2022-06-29 11:36:55 -0300

    refatoração e correção de complexidade requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,36 +1,49 @@
-// Desafio 11
-function generatePhoneNumber(numbersArray) {
+function validacaoNumero(numbersArray) {
+  for (let index = 0; index < numbersArray.length; index += 1) {
+    if (numbersArray[index] < 0 || numbersArray[index] > 9) {
+      return false;
+    }
+  }
+  return true;
+}
+function validacaoRepeticao(numbersArray) {
+  let countObj = {};
+  numbersArray.forEach((element) => {
+    countObj[element] = (countObj[element] || 0) + 1;
+  });
+  for (let key in countObj) {
+    if (countObj[key] >= 3) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function validacaoTamanho(numbersArray) {
   if (numbersArray.length !== 11) {
+    return false;
+  }
+  return true;
+}
+function validacaoGeral(numbersArray) {
+  if (!validacaoTamanho(numbersArray)) {
     return 'Array com tamanho incorreto.';
   }
-
-  for (let index = 0; index < numbersArray.length; index += 1) { // validação
-    if (numbersArray[index] < 0 || numbersArray[index] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
+  if (!validacaoNumero(numbersArray) || !validacaoRepeticao(numbersArray)) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return true;
+}
+// Desafio 11
+function generatePhoneNumber(numbersArray) {
+  let validacao = validacaoGeral(numbersArray);
+  if (validacao !== true) {
+    return validacao;
   }
 
-  let count = {}; // contagem de cada numero
-  numbersArray.forEach((element) => {
-    count[element] = (count[element] || 0) + 1;
-  });
-  for (let key in count) { // validação
-    if (count[key] >= 3) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
-  }
-  // caso o array esteja de acordo
-  let phoneNumber = '';
-  for (let index = 0; phoneNumber.length < 15; index += 1) {
-    if (index === 0) {
-      phoneNumber += '(';
-    } else if (index === 2) {
-      phoneNumber += ') ';
-    } else if (index === 7) {
-      phoneNumber += '-';
-    }
-    phoneNumber += numbersArray[index];
-  }
+  let toString = numbersArray.join('');
+  let grupo = toString.match(/(\d{2})(\d{5})(\d{4})/);
+  let phoneNumber = `(${grupo[1]}) ${grupo[2]}-${grupo[3]}`;
   return phoneNumber;
 }
 
@@ -47,12 +60,6 @@ function triangleCheck(lineA, lineB, lineC) {
 
 // Desafio 13
 function hydrate(string) {
-  /* [COLINHA]
-    \d+ -> procura por um digito ou mais
-    flag g : Perform a global match (find all matches rather than stopping after the first match);
-    match() -> faz uma busca pelo parâmetro
-   */
-
   let digitFinder = /\d+/g;
   let digitsArray = string.match(digitFinder);
   let sum = 0;
